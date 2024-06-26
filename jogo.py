297a039b35971b0c862e761eb957d93384d7079f
Denise Lameu <104370411+deniselameu@users.noreply.github.com>
2024-06-26 12:30:12 -0300

    jogoSecreto.py
    Jogo básico com comandos de (Funções, If e While), onde o usuário chuta números aleatórios até acertar o número escolhido pela máquina.

--- jogo.py
+++ jogo.py
@@ -3,7 +3,7 @@ print('Bem-Vindo ao jogo Secreto! ')
 
 def gerar_numero_aleatorio():
  import random
- return random.randrange(1, 10)
+ return random.randrange(1, 100)
 
 numero_secreto = gerar_numero_aleatorio()
 #print(f'O número secreto é {numero_secreto}')
@@ -17,7 +17,7 @@ def verificar_numero_escolhido():
  tentativas = 0
  
  while True:
-  numero_escolhido = int(input('Escolha um número de 1 a 10: '))
+  numero_escolhido = int(input('Escolha um número de 1 a 100: '))
   tentativas += 1
   if(numero_escolhido == numero_secreto):
    break
